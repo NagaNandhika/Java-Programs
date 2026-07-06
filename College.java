cd1844ac4826275d1847152fd142541d331b980b
Nandhika <115523710+NagaNandhika@users.noreply.github.com>
2026-07-06 07:38:42 +0530

    Partial abstraction

--- College.java
+++ College.java
@@ -1,49 +1,23 @@
-package org.fully;
-//child class
-//Fully abstraction
-//Multiple Inheritance
-//Hybrid inheritance
-public class College extends Bus implements Student,Staff {
+package org.abs;
+
+public class College extends Student {
 
 	@Override
 	public void stuId() {
-		System.out.println("The student id is 6767");
-		
-	}
-
-	@Override
-	public void stuName() {
-		System.out.println("The student name is Ram");
+		System.out.println("The student id is 7878");
 		
 	}
 	public static void main(String[] args) {
 		College c = new College();
 		c.stuId();
 		c.stuName();
-		c.staffId();
-		c.staffName();
-		c.busFee();
-	}
-
-	@Override
-	public void staffId() {
-		System.out.println("The staff id 67676");
-		
-	}
-
-	@Override
-	public void staffName() {
-		
-		System.out.println("The staff name is Manoj");
 	}
 
 	
-	
-
-	
 
 	
 	
-	
 
+	
+	
 }
--- Student.java
+++ Student.java
@@ -1,7 +1,13 @@
-package org.fully;
-//parent class
-public interface Student {
+package org.abs;
+//Partial abstraction
+public abstract class Student {
+	//abstract method
 	public abstract void stuId();
-	void stuName();
-	
+	//non abstract method
+	public void stuName() {
+		System.out.println("The student name is Teena");
+	}
+		
+
+
 }
